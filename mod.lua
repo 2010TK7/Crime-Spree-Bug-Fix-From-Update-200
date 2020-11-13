c55b1a7cdf8743a8256b1602b785b7e87c62631f
TK7 <64648487+2010TK7@users.noreply.github.com>
2020-11-13 11:14:44 +0800

    rename.
    Crime Spree Bug Fix From Update 200

--- mod.lua
+++
@@ -1,9 +0,0 @@
-local OldFunc = InfamyTreeGui._setup
-function InfamyTreeGui:_setup()
-	local OldExtraFunc =  managers.crime_spree.in_progress
-	function managers.crime_spree:in_progress()
-		return false
-	end
-	OldFunc(self)
-	managers.crime_spree.in_progress = OldExtraFunc
-end
