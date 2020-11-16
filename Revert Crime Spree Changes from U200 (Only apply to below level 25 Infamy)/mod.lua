baeb10d02bdbc4440a68c07a3f8dc60c18bb099f
TK7 <64648487+2010TK7@users.noreply.github.com>
2020-11-16 11:07:03 +0800

    CSBFFU200 2.1

--- Revert Crime Spree Changes from U200 (Only apply to below level 25 Infamy)/mod.lua
+++ Revert Crime Spree Changes from U200 (Only apply to below level 25 Infamy)/mod.lua
@@ -2,7 +2,7 @@ local OldFunc = InfamyTreeGui._setup
 function InfamyTreeGui:_setup()
 	local OldExtraFunc =  managers.crime_spree.in_progress
 	function managers.crime_spree:in_progress()
-		return false
+		return managers.experience:current_rank() >= 25 and OldExtraFunc(self)
 	end
 	OldFunc(self)
 	managers.crime_spree.in_progress = OldExtraFunc
